789332dfa5293d90db58c9a19cc81bc2f8961fca
Nanthagopal98 <nanthagopal.sj@gmail.com>
2022-05-19 19:53:12 +0530

    [Nantha] - Fibonacci Series Added

---
+++ logicalPrograms/logicalPrograms/fibonacciSeries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logicalPrograms
+{
+    public class fibonacciSeries
+    {
+        public void fibonacci()
+        {
+            int x=0, y=1,z,temp2;
+            Console.WriteLine("Enter value to find Fibonacci series");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Fibonacci series of "+input+" values");
+            for (int i = 0; i < input; i++)
+            {
+                z = x + y;
+                x = y; 
+                y = z;
+                Console.WriteLine(z);
+
+            }
+        }
+    }
+}
