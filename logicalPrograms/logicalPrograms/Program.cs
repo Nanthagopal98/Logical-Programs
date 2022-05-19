7586adcbf1ebe3e47e4e2bf73a2fea8972ae17ec
Nanthagopal98 <nanthagopal.sj@gmail.com>
2022-05-19 20:32:19 +0530

    Perfect Number Added

--- logicalPrograms/logicalPrograms/Program.cs
+++ logicalPrograms/logicalPrograms/Program.cs
@@ -1,4 +1,17 @@
 ﻿
 // See https://aka.ms/new-console-template for more information
-logicalPrograms.fibonacciSeries run = new logicalPrograms.fibonacciSeries();
-run.fibonacci();
+Console.WriteLine("Enter 1 to run fibonacciSeries");
+Console.WriteLine("Enter 2 to run perfectNumber");
+int run =Convert.ToInt32(Console.ReadLine());
+switch (run)
+{
+    case 1:
+        logicalPrograms.fibonacciSeries run1 = new logicalPrograms.fibonacciSeries();
+        run1.fibonacci();
+        break;
+    case 2:
+        logicalPrograms.perfectNumber run2 = new logicalPrograms.perfectNumber();
+        run2.findPerfectNumber();
+        break;
+}
+
---
+++ logicalPrograms/logicalPrograms/perfectNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logicalPrograms
+{
+    public class perfectNumber
+    {
+        public void findPerfectNumber()
+        {
+            int result = 0;
+            Console.WriteLine("Enter number to check for perfect number");
+            int input = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < input; i++)
+            {
+                if (input % i == 0)
+                {
+                    Console.WriteLine(i);
+                    result = result + i;
+
+                }
+            }
+            if(result == input)
+            {
+                Console.WriteLine(input + " is a perfect number");
+            }
+            else
+            {
+                Console.WriteLine(input + " is not a perfect number");
+            }
+        }
+    }
+}
